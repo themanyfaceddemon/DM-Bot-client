faa4a5ce4649c6cda2ca538622a29096d348e435
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-07-15 00:52:46 +0300

    fuck console menu

--- console client/console_menu.py
+++ console client/console_menu.py
@@ -4,20 +4,47 @@ from art import tprint
 def menu_address():
     print("\n1. Ввести адрес")
     print("2. Завершить программу\n")
-    choice = int(input("Введите действие: "))
+    try:
+        choice = int(input("Введите действие: "))
+        if choice not in (1, 2):
+            print("Выбрано неверное действие!")
+            return menu_address()
+        
+    except ValueError:
+        print("Некорректный ввод. Введите число.")
+        return  menu_address()
+    
     return choice
 
 def menu_auth():
     print("\n1. Зарегистрироваться")
     print("2. Авторизоваться")
     print("3. Выйти\n")
-    choice = int(input("Введите действие: "))
+    try:
+        choice = int(input("Введите действие: "))
+        if choice not in range(1, 4):
+            print("Выбрано неверное действие!")
+            return menu_auth()
+        
+    except ValueError:
+        print("Некорректный ввод. Введите число.")
+        return  menu_auth()
+    
     return choice
 
 def dm_menu():
     print("\n1. Выйти с системы")
     print("2. Сменить пароль\n")
-    choice = int(input("Введите действие: "))
+    try:
+        choice = int(input("Введите действие: "))
+        if choice not in (1, 2):
+            print("Выбрано неверное действие!")
+            return dm_menu()
+        
+    except ValueError:
+        print("Некорректный ввод. Введите число.")
+        return  dm_menu()
+    
     return choice
 
 
