c2d8c138016aab67316b926195e8e9b857e83728
Nix Silvam <nixsilvam@icloud.com>
2024-07-15 01:37:40 +0300

    setup_server_content

---
+++ console client/setup_server_content.py
@@ -0,0 +1,18 @@
+from server_system_setup import server_system
+import os
+import zipfile
+
+
+def setup_server_content() -> None:
+    
+    """Проверяет наличие папки Sprites в корневом каталоге. При отсутствии- вызывает функцию 
+    server_system.download_server_texture() и распаковывает архив по пути результата этой функции.
+    """
+    root_dir = os.path.abspath(os.path.dirname(__file__))
+    sprites_dir = os.path.join(root_dir, 'Sprites')
+    if not os.path.exists(sprites_dir):
+        zip_path = server_system.download_server_texture()
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(sprites_dir)    
+
+    
