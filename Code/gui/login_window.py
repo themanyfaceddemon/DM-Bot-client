c4d5ebc7785fd798c8e8ed749b2a741aec381c87
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-28 15:52:43 +0300

    Немного говна

--- Code/gui/login_window.py
+++ Code/gui/login_window.py
@@ -1,6 +1,7 @@
 import os
 
 from gui.main_window import MainApplicationWindow
+from PyQt6.QtCore import QThread, pyqtSignal
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import (QDialog, QLabel, QLineEdit, QMainWindow,
                              QMessageBox, QProgressBar, QPushButton)
@@ -8,7 +9,7 @@ from PyQt6.uic import loadUi
 from root_path import ROOT_PATH
 from systems.decorators import global_class
 from systems.network import ClientUnit
-from PyQt6.QtCore import QThread, pyqtSignal
+
 
 @global_class
 class LoginWindow(QMainWindow):
